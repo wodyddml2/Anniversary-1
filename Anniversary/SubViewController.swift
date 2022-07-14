cf99bd12b22b0acc65578addb6d05b548ba6b147
wodyddml2 <wodyddml2@naver.com>
2022-07-14 21:27:24 +0900

    class 사용하여 검색창

--- Anniversary/SubViewController.swift
+++ Anniversary/SubViewController.swift
@@ -9,30 +9,40 @@ import UIKit
 
 class SubViewController: UIViewController, UITextFieldDelegate {
 
-    class Anniversary {
-        var one = "오늘은 널 만난지 100일"
-        var two = "오늘은 널 만난지 200일"
-        var three = "오늘은 널 만난지 300일"
-        var four = "오늘은 널 만난지 400일"
-        
+    class Diary {
+//        var one = "오늘은 널 만난지 100일"
+//        var two = "오늘은 널 만난지 200일"
+//        var three = "오늘은 널 만난지 300일"
+//        var four = "오늘은 널 만난지 400일"
+        var diary: [String] = []
        
         func anniversary(_ a: UILabel) {
             a.text = ""
             a.textColor = UIColor.darkGray
             a.numberOfLines = 0
         }
-        override func anniversary(_ a: UILabel) {
-            
+        func anniversary(_ a: UILabel, _ b: UITextField) {
+            guard let diaryText = b.text else {
+                return
+            }
+            self.diary.append(diaryText)
+            a.text = ""
+            for i in 0...self.diary.count - 1 {
+                
+                a.text! += self.diary[i]
+            }
         }
         
     }
-   
+    let firstDiary = Diary()
+    
     @IBOutlet weak var dateSearch: UITextField!
     @IBOutlet weak var dateLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         dateSearch.delegate = self
         
+        firstDiary.anniversary(dateLabel)
         
       
     }
@@ -40,6 +50,7 @@ class SubViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         dateSearch.resignFirstResponder()
+        firstDiary.anniversary(dateLabel, dateSearch)
         return true
     }
     
